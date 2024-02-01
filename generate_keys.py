4e0f2543b9235e300adb49dd088ac4cbea948621
bwithai <sanaullah41821@gmail.com>
2024-02-01 09:54:18 +0500

    generate pub/pvt keys

---
+++ generate_keys.py
@@ -0,0 +1,13 @@
+import paramiko
+
+key_path = "/home/bwithai/.ssh/01_id"
+
+# Generate an RSA key pair
+key = paramiko.RSAKey.generate(2048)
+
+# Save the private key
+key.write_private_key_file(key_path)
+
+# Save the public key (optional)
+with open(key_path + '.pub', 'w') as f:
+    f.write(f'{key.get_name()} {key.get_base64()} your_comment')
